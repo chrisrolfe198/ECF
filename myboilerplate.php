f4370f50eccc3af77a451629154b38cf6658b024
Christopher Rolfe <christopher.rolfe198@gmail.com>
2013-11-04 13:30:26 +0000

    Base less files, docs, and modules

---
+++ myboilerplate.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- Optional Doctype - test with and without -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Your Message Subject or Title</title>
+	<style type="text/css">
+		.ExternalClass {width:100%;}
+		.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+			line-height: 100%;
+		}
+		body {-webkit-text-size-adjust:none; -ms-text-size-adjust:none;}
+		body {margin:0; padding:0;}
+		table td {border-collapse:collapse;}    
+		p {margin:0; padding:0; margin-bottom:0;}
+		h1, h2, h3, h4, h5, h6 {
+			color: black; 
+			line-height: 100%; 
+		} 
+		a, a:link {
+			color:#2A5DB0;
+			text-decoration: underline;
+		}
+		/*Don't edit above styles*/
+		body, #body_style {
+			background:#808080;
+			min-height:1000px;
+			color:#000;
+			font-family:Arial, Helvetica, sans-serif;
+			font-size:12px;
+		}
+		span.yshortcuts { color:#000; background-color:none; border:none;}
+		span.yshortcuts:hover,
+		span.yshortcuts:active,
+		span.yshortcuts:focus {color:#000; background-color:none; border:none;}
+		a:visited { color: #3c96e2; text-decoration: none} 
+		a:focus   { color: #3c96e2; text-decoration: underline}  
+		a:hover   { color: #3c96e2; text-decoration: underline}
+		/*My styles*/
+		/* My variables redeclared to stop breaking the above */
+		#body_style * {
+			padding: 0;
+			margin: 0;
+			text-align: left;
+			font-weight: 100;
+			vertical-align: top;
+		}
+		#body_style .body {
+			margin: 0 auto;
+		}
+		#body_style h1 {
+			font-size: 36px;
+		}
+		#body_style h2 {
+			font-size: 23px;
+		}
+		#body_style h3 {
+			font-size: 21px;
+		}
+		#body_style h4 {
+			font-size: 18px;
+		}
+		#body_style h5 {
+			font-size: 14px;
+		}
+		#body_style h6 {
+			font-size: 12px;
+		}
+		#body_style h1,
+		#body_style h2,
+		#body_style h3,
+		#body_style h4,
+		#body_style h5,
+		#body_style h6,
+		#body_style p,
+		#body_style img {
+			padding: 5px;
+		}
+		#body_style p {
+			font-size: 14px;
+		}
+		#body_style ol,
+		#body_style ul {
+			padding-left: 30px;
+			padding-right: 0;
+		}
+		#body_style ol li,
+		#body_style ul li {
+			padding-top: 1px;
+			padding-bottom: 1px;
+		}
+		#body_style img {
+			border-top-left-radius: 3px;
+			border-bottom-left-radius: 3px;
+			border-top-right-radius: 3px;
+			border-bottom-right-radius: 3px;
+		}
+		#body_style .section {
+			background-color: white;
+			border-top-left-radius: 3px;
+			border-bottom-left-radius: 3px;
+			border-top-right-radius: 3px;
+			border-bottom-right-radius: 3px;
+			margin-top: 5px;
+		}
+		#body_style .section tbody > tr > td > img {
+			border-top-left-radius: 3px;
+			border-bottom-left-radius: 0;
+			border-top-right-radius: 0;
+			border-bottom-right-radius: 0;
+		}
+		#body_style .section tbody > tr:last-child > td {
+			border-top-left-radius: 0;
+			border-bottom-left-radius: 3px;
+			border-top-right-radius: 0;
+			border-bottom-right-radius: 3px;
+		}
+		#body_style .section .third {
+			width: 200px;
+		}
+		#body_style .section .half-width {
+			width: 300px;
+		}
+		#body_style .section .two-third {
+			width: 400px;
+		}
+		#body_style .section .two-third p,
+		#body_style .section .two-third h1,
+		#body_style .section .two-third h2,
+		#body_style .section .two-third h3,
+		#body_style .section .two-third h4,
+		#body_style .section .two-third h5,
+		#body_style .section .two-third h6 {
+			padding-left: 5px;
+			padding-right: 5px;
+		}
+		#body_style .section .full-width {
+			width: 600px;
+		}
+		#body_style .spacing-row {
+			height: 5px;
+		}
+		#body_style .black-and-white {
+			background-color: black;
+			color: white;
+			padding-top: 5px;
+			padding-bottom: 5px;
+			padding-left: 5px;
+			padding-right: 5px;
+			min-height: 5px;
+		}
+		#body_style .banner-image {
+			padding: 0;
+		}
+
+	</style>
+</head>
+<body style="background:#808080; min-height:1000px; color:#000;font-family:Arial, Helvetica, sans-serif; font-size:12px"
+alink="#FF0000" link="#FF0000" bgcolor="#808080" text="#000" yahoo="fix">
+<div id="body_style" style="padding:15px"><!-- Body fake -->
+	<table cellpadding="0" cellspacing="0" border="0" width="600" align="center" class="body"> <!-- Table for the body -->
+		<tbody>
+			<tr>
+				<td>
+					<?php include 'modules/header.html'; ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="spacing-row"></td>
+			</tr>
+			<tr>
+				<td>
+					<?php include 'modules/two_column.html'; ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="spacing-row"></td>
+			</tr>
+			<td>
+				<?php include 'modules/one_column.html'; ?>
+			</td>
+		</tr>
+	</td>
+</tr>
+</tbody>
+</table> 
+</div>
+</body>
