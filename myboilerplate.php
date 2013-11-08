440b00a8cc1ab498efbca248dcab44c76edcea55
Christopher Rolfe <christopher.rolfe198@gmail.com>
2013-11-08 11:45:08 +0000

    Updates to the template

--- myboilerplate.php
+++ myboilerplate.php
@@ -4,47 +4,9 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Your Message Subject or Title</title>
+	<meta name="viewport" content="width=device-width, initial scale=1.0"/>
 	<style>
-		.ExternalClass{width:100%;}
-		.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:100%;}
-		body{-webkit-text-size-adjust:none;-ms-text-size-adjust:none;}
-		body{margin:0;padding:0;}
-		table td{border-collapse:collapse;}
-		p{margin:0;padding:0;margin-bottom:0;}
-		h1,h2,h3,h4,h5,h6{color:black;line-height:100%;}
-		a,a:link{color:#2A5DB0;text-decoration:underline;}
-		#body_style{}#body_style *{padding:0;margin:0;text-align:left;font-weight:100;vertical-align:top;}
-		#body_style .body{width:596px;margin:0 auto;color:#ffffff;font-size:16px;background-color:#232020;}
-		#body_style h1{font-size:41px;}
-		#body_style h2{font-size:34px;}
-		#body_style h3{font-size:28px;}
-		#body_style h4{font-size:20px;}
-		#body_style h5{font-size:16px;}
-		#body_style h6{font-size:14px;}
-		#body_style h1,#body_style h2,#body_style h3,#body_style h4,#body_style h5,#body_style h6,#body_style p,#body_style img{padding:5px;}
-		#body_style p{font-size:16px;}
-		#body_style ol,#body_style ul{padding-left:30px;padding-right:0;}#body_style ol li,#body_style ul li{padding-top:1px;padding-bottom:1px;}
-		#body_style img{border-top-left-radius:3px;border-bottom-left-radius:3px;border-top-right-radius:3px;border-bottom-right-radius:3px;}
-		#body_style .full-width{width:596px;}
-		#body_style .inner-width{width:581px;}
-		#body_style .third{width:193.66666666666666px;}
-		#body_style .two-third{width:387.3333333333333px;}#body_style .two-third p,#body_style .two-third h1,#body_style .two-third h2,#body_style .two-third h3,#body_style .two-third h4,#body_style .two-third h5,#body_style .two-third h6{padding-left:5px;padding-right:5px;}
-		#body_style .half-width{width:290.5px;}
-		#body_style .spacing-row-small{height:5px;}
-		#body_style .spacing-row-large{height:10px;}
-		#body_style .tb-body-padding{height:15px;}
-		#body_style .lr-body-padding{width:15px;}
-		#body_style .section{background-color:white;border-top-left-radius:3px;border-bottom-left-radius:3px;border-top-right-radius:3px;border-bottom-right-radius:3px;margin-top:5px;}#body_style .section tbody>tr>td>img{border-top-left-radius:3px;border-bottom-left-radius:0;border-top-right-radius:0;border-bottom-right-radius:0;}
-		#body_style .section tbody>tr:last-child>td{border-top-left-radius:0;border-bottom-left-radius:3px;border-top-right-radius:0;border-bottom-right-radius:3px;}
-		#body_style .black-and-white{background-color:black;color:white;padding-top:5px;padding-bottom:5px;padding-left:5px;padding-right:5px;min-height:5px;}
-		#body_style .banner-image{padding:0;}
-		#body_style h2{color:#ffffff;}
-		#body_style strong{font-weight:800;}
-		#body_style *{text-align:center;}
-		#body_style .body-copy p,#body_style .body-copy h2{color:#ffffff;}
-		#body_style .body-copy .heading-caption{margin-bottom:0;padding-bottom:0;font-size:20px;}
-		#body_style .body-copy .caption{margin-top:0;padding-top:0;font-size:14px;}
-		#body_style .body-copy .emphasis{font-size:20px;}
+		.ExternalClass{width:100%}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:100%}body{-webkit-text-size-adjust:none;-ms-text-size-adjust:none}body{margin:0;padding:0}table td{border-collapse:collapse}p{margin:0;padding:0;margin-bottom:0}h1,h2,h3,h4,h5,h6{color:#000;line-height:100%}a,a:link{color:#2a5db0;text-decoration:underline}#body_style *{padding:0;margin:0;text-align:left;font-weight:100;vertical-align:top}#body_style .body{width:596px;margin:0 auto;color:#fff;font-size:16px;background-color:#232020}#body_style h1{font-size:41px}#body_style h2{font-size:34px}#body_style h3{font-size:28px}#body_style h4{font-size:20px}#body_style h5{font-size:16px}#body_style h6{font-size:14px}#body_style h1,#body_style h2,#body_style h3,#body_style h4,#body_style h5,#body_style h6,#body_style p,#body_style img{padding:5px}#body_style p{font-size:16px}#body_style ol,#body_style ul{padding-left:30px;padding-right:0}#body_style ol li,#body_style ul li{padding-top:1px;padding-bottom:1px}#body_style img{border-top-left-radius:3px;border-bottom-left-radius:3px;border-top-right-radius:3px;border-bottom-right-radius:3px}#body_style .full-width{width:596px}#body_style .inner-width{width:581px}#body_style .third{width:193.66666666666666px}#body_style .two-third{width:387.3333333333333px}#body_style .two-third p,#body_style .two-third h1,#body_style .two-third h2,#body_style .two-third h3,#body_style .two-third h4,#body_style .two-third h5,#body_style .two-third h6{padding-left:5px;padding-right:5px}#body_style .half-width{width:290.5px}#body_style .spacing-row-small{height:5px}#body_style .spacing-row-large{height:10px}#body_style .tb-body-padding{height:15px}#body_style .lr-body-padding{width:15px}#body_style .section{background-color:#fff;border-top-left-radius:3px;border-bottom-left-radius:3px;border-top-right-radius:3px;border-bottom-right-radius:3px;margin-top:5px}#body_style .section tbody>tr>td>img{border-top-left-radius:3px;border-bottom-left-radius:0;border-top-right-radius:0;border-bottom-right-radius:0}#body_style .section tbody>tr:last-child>td{border-top-left-radius:0;border-bottom-left-radius:3px;border-top-right-radius:0;border-bottom-right-radius:3px}#body_style .black-and-white{background-color:#000;color:#fff;padding-top:5px;padding-bottom:5px;padding-left:5px;padding-right:5px;min-height:5px}#body_style .banner-image{padding:0}#body_style h2{color:#fff}#body_style strong{font-weight:800}#body_style *{text-align:center}#body_style .body-copy p,#body_style .body-copy h2{color:#fff}#body_style .body-copy .heading-caption{margin-bottom:0;padding-bottom:0;font-size:20px}#body_style .body-copy .caption{margin-top:0;padding-top:0;font-size:14px}#body_style .body-copy .emphasis{font-size:20px}#body_style .lr-table-padding{width:15px}#body_style .tb-table-padding{height:15px}
 	</style>
 </head>
 <body style="background:#808080; color:#000;font-family:Arial, Helvetica, sans-serif; font-size:12px"
